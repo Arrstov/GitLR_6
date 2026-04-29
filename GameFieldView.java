21b2493240890ea9e87ca72fdedaf1f9d0aa0354
Михаил Ли <miha0095.li@gmail.com>
2026-04-29 21:58:38 +0300

    коммит 3 в main

--- GameFieldView.java
+++ GameFieldView.java
@@ -7,7 +7,7 @@ import game.model.Tile;
 import game.model.FreezeMine;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.*; // коммит 3 main
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.HashMap;
